a7e956f5944b71ba7da6ff9d442e187c0f7e4ed5
Alex Zhukovich <zhukovich.a@gmail.com>
2025-07-04 22:46:33 +0200

    Refactored ActionCategoryDetails class; Renamed CategoryWithActionMapper to ActionCategoryDetailsMapper

--- MoodTracker/app/src/main/java/com/alexzh/moodtracker/data/mapper/ActionCategoryDetailsMapper.kt
+++ MoodTracker/app/src/main/java/com/alexzh/moodtracker/data/mapper/ActionCategoryDetailsMapper.kt
@@ -1,11 +1,17 @@
 package com.alexzh.moodtracker.data.mapper
 
 import com.alexzh.moodtracker.data.database.action.ActionCategoryDetailsEntity
+import com.alexzh.moodtracker.domain.PastelAccentColor
 import com.alexzh.moodtracker.domain.model.ActionCategoryDetails
 
 fun ActionCategoryDetailsEntity.toDomain(): ActionCategoryDetails {
     return ActionCategoryDetails(
-        category = category.toDomain(),
+        id = category.id,
+        name = category.name,
+        color = PastelAccentColor.getPastelAccentColorsByName(
+            category.colorName,
+            defaultColor = PastelAccentColor.GREY
+        ),
         actions = actions.toDomainList()
     )
 }
--- MoodTracker/app/src/main/java/com/alexzh/moodtracker/domain/model/ActionCategoryDetails.kt
+++ MoodTracker/app/src/main/java/com/alexzh/moodtracker/domain/model/ActionCategoryDetails.kt
@@ -1,7 +1,11 @@
 package com.alexzh.moodtracker.domain.model
 
+import com.alexzh.moodtracker.domain.PastelAccentColor
+
 data class ActionCategoryDetails(
-    val category: ActionCategory,
+    val id: Long,
+    val name: String,
+    val color: PastelAccentColor,
     val actions: List<Action>
 ) {
     val actionsCount: Int get() = actions.size
