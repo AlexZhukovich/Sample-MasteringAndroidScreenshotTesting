d5feaf61c87c0f552ac69a9932b13ad91f3f72ec
Alex Zhukovich <zhukovich.a@gmail.com>
2025-07-23 22:28:32 +0200

    Replace emojis to icons

--- MoodTracker/app/src/main/java/com/alexzh/moodtracker/ui/feature/home/HomeScreen.kt
+++ MoodTracker/app/src/main/java/com/alexzh/moodtracker/ui/feature/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.alexzh.moodtracker.ui.feature.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -31,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -160,12 +163,13 @@ private fun MoodItemCard(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    Text(
-                        text = moodItem.mood.emoji,
-                        style = MaterialTheme.typography.headlineMedium
+                    Image(
+                        modifier = Modifier.size(42.dp),
+                        painter = painterResource(moodItem.mood.icon),
+                        contentDescription = stringResource(moodItem.mood.label),
                     )
                     Text(
-                        text = moodItem.mood.title,
+                        text = stringResource(moodItem.mood.label),
                         style = MaterialTheme.typography.titleMedium,
                         fontWeight = FontWeight.Bold
                     )
--- MoodTracker/app/src/main/java/com/alexzh/moodtracker/ui/model/LocalizedMood.kt
+++ MoodTracker/app/src/main/java/com/alexzh/moodtracker/ui/model/LocalizedMood.kt
@@ -1,20 +1,21 @@
 package com.alexzh.moodtracker.ui.model
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.alexzh.moodtracker.R
 
-enum class LocalizedMood(val happiness: Float, @StringRes val titleRes: Int, val emoji: String) {
-    ANGRY(1.0f, R.string.mood_angry, "😠"),
-    SAD(2.0f, R.string.mood_sad, "😢"),
-    OK(3.0f, R.string.mood_ok, "😐"),
-    GOOD(4.0f, R.string.mood_good, "😊"),
-    HAPPY(5.0f, R.string.mood_happy, "😄");
-
-    val title: String
-        @Composable
-        get() = stringResource(titleRes)
+enum class LocalizedMood(
+    val happiness: Float,
+    @StringRes val label: Int,
+    @DrawableRes val icon: Int
+) {
+    ANGRY(1.0f, R.string.mood_angry, R.drawable.ic_mood_angry_colorful),
+    SAD(2.0f, R.string.mood_sad, R.drawable.ic_mood_sad_colorful),
+    OK(3.0f, R.string.mood_ok, R.drawable.ic_mood_ok_colorful),
+    GOOD(4.0f, R.string.mood_good, R.drawable.ic_mood_good_colorful),
+    HAPPY(5.0f, R.string.mood_happy, R.drawable.ic_mood_happy_colorful);
 
     companion object {
         fun fromHappiness(happiness: Float, default: LocalizedMood = OK): LocalizedMood =
