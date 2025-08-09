f1439385174c2e974c7c290273de77ddadeb7c48
Alex Zhukovich <zhukovich.a@gmail.com>
2025-08-09 22:21:29 +0200

    Added ActionToHappinessChart and AverageDailyMoodChart components

---
+++ MoodTracker/app/src/main/java/com/alexzh/moodtracker/ui/designsystem/chart/ChartDataItem.kt
@@ -0,0 +1,6 @@
+package com.alexzh.moodtracker.ui.designsystem.chart
+
+data class ChartDataItem(
+    val label: String,
+    val value: Float
+)
---
+++ MoodTracker/app/src/main/java/com/alexzh/moodtracker/ui/designsystem/core/painter/Painters.kt
@@ -0,0 +1,21 @@
+package com.alexzh.moodtracker.ui.designsystem.core.painter
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+
+fun Painter.toImageBitmap(
+    size: Size,
+    density: Density,
+    layoutDirection: LayoutDirection,
+): ImageBitmap {
+    val bitmap = ImageBitmap(size.width.toInt(), size.height.toInt())
+    val canvas = androidx.compose.ui.graphics.Canvas(bitmap)
+    CanvasDrawScope().draw(density, layoutDirection, canvas, size) {
+        draw(size)
+    }
+    return bitmap
+}
