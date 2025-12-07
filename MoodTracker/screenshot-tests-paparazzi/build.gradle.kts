5ad4d4b133e2dc08c73ae8f99fe6f5245011e9f3
Alex Zhukovich <zhukovich.a@gmail.com>
2025-12-07 17:47:32 +0100

    Added screenshot-tests-paparazzi module for screenshot test cases implemented using Paparazzi

---
+++ MoodTracker/screenshot-tests-paparazzi/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.paparazzi)
+}
+
+android {
+    namespace = "com.alexzh.moodtracker.screenshottests.paparazzi"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":common-ui"))
+    implementation(project(":design-system"))
+    implementation(project(":feature-action-management"))
+    implementation(project(":feature-home"))
+    implementation(project(":feature-settings"))
+    implementation(project(":feature-statistics"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    testImplementation(libs.junit)
+
+    debugImplementation(libs.androidx.ui.tooling)
+}
